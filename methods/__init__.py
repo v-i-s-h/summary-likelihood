60360ae72060e3527e4d090f517900979cff2a37
v-i-s-h <get_vichu@yahoo.com>
2022-07-31 15:45:06 +0300

    label smoothing

--- methods/__init__.py
+++ methods/__init__.py
@@ -1,10 +1,13 @@
+from methods.labelsmoothing import LabelSmoothing
 from .mfvi import MFVI
 from .sl import SummaryLikelihood
 from .slim import SummaryLikelihoodIm
 from .pl import PredictionLikelihood
+from .labelsmoothing import LabelSmoothing
 
 # For lookup
 mfvi = MFVI
 sl = SummaryLikelihood
 slim = SummaryLikelihoodIm
 pl = PredictionLikelihood
+ls = LabelSmoothing
