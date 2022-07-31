04999439f833d6f3eb1173aaf433dda6b9da0905
v-i-s-h <get_vichu@yahoo.com>
2022-07-31 15:45:39 +0300

    LS experiments job scripts

---
+++ slurm-scripts/submit_mnistc_ls.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#SBATCH --time=0:40:00
+#SBATCH --mem-per-cpu=4G
+#SBATCH --gres=gpu:1
+#SBATCH --exclude=dgx[1-7]
+#SBATCH --array=1-5
+#SBATCH --output=logs/job-%A-%a.out
+#SBATCH --error=logs/job-%A-%a.err
+
+module purge
+module load miniconda
+source activate bayes
+
+export DISABLE_PBAR=1
+
+OUTDIR="zoo/bmnist53-ls"
+MAX_STEPS=3000
+METHOD="ls"
+
+# for dssize in 10000 1000
+# do
+#     python train.py \
+#         --method $METHOD \
+#         --dataset BinaryMNISTC --ds-params size=$dssize,labels=53,corruption=identity \
+#         --model LeNet \
+#         --max-steps $MAX_STEPS \
+#         --batch-size 256 \
+#         --mc-samples 32 \
+#         --outdir $OUTDIR \
+#         --prefix $METHOD-sz$dssize-$SLURM_ARRAY_TASK_ID
+# done
+
+for dssize in 10000 1000
+do
+    python train.py \
+        --method $METHOD \
+        --dataset BinaryMNISTC --ds-params size=$dssize,labels=53,corruption=identity \
+        --model ConvNet \
+        --max-steps $MAX_STEPS \
+        --batch-size 256 \
+        --mc-samples 32 \
+        --outdir $OUTDIR \
+        --prefix $METHOD-sz$dssize-$SLURM_ARRAY_TASK_ID
+done
