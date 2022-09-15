48b49e2af7f8c13c8610905c0633d865d41a4701
v-i-s-h <get_vichu@yahoo.com>
2022-09-15 12:03:47 +0300

    slurm scripts updates

--- slurm-scripts/submit_mnistc_eval.sh
+++ slurm-scripts/submit_mnistc_eval.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --time=3:00:00
+#SBATCH --time=12:00:00
 #SBATCH --mem-per-cpu=4G
 #SBATCH --gres=gpu:1
 #SBATCH --exclude=dgx[1-7]
@@ -35,29 +35,29 @@ case $SLURM_ARRAY_TASK_ID in
 esac
 
 
-# # -------------------------------- SL (auto prior) -----------------------------
-# OUTDIR="zoo/sl/auto-prior-alphavar"
-# METHOD="sl"
-# for alpha in 0.01 0.1 1.0 10.0 100.0 0.05 0.5 5.0 25.0 50.0 75.0
-# do
-#     for dssize in 8000 1000
-#     do
-#         alpha_part=`printf '%1.0e' $alpha`
+# -------------------------------- SL (auto prior) -----------------------------
+OUTDIR="zoo/sl/auto-prior-alphavar"
+METHOD="sl"
+for alpha in 0.01 0.1 1.0 10.0 100.0 0.05 0.5 5.0 25.0 50.0 75.0
+do
+    for dssize in 8000 1000
+    do
+        alpha_part=`printf '%1.0e' $alpha`
 
-#         # LeNet
-#         echo "--------------- SL: LeNet, alpha = ${alpha_part}, SZ = ${dssize} ------------"
-#         python eval_calib.py \
-#             --corruption $CORRUPTION \
-#             --models $OUTDIR/BinaryMNISTC-${dssize}-53-identity/LeNet/$METHOD-alpha$alpha_part-sz$dssize-*
+        # LeNet
+        echo "--------------- SL: LeNet, alpha = ${alpha_part}, SZ = ${dssize} ------------"
+        python eval_calib.py \
+            --corruption $CORRUPTION \
+            --models $OUTDIR/BinaryMNISTC-${dssize}-53-identity/LeNet/$METHOD-alpha$alpha_part-sz$dssize-*
         
-#         # ConvNet
-#         echo "--------------- SL: ConvNet, alpha = ${alpha_part}, SZ = ${dssize} ------------"
-#         python eval_calib.py \
-#             --corruption $CORRUPTION \
-#             --models $OUTDIR/BinaryMNISTC-${dssize}-53-identity/ConvNet/$METHOD-alpha$alpha_part-sz$dssize-*
+        # ConvNet
+        echo "--------------- SL: ConvNet, alpha = ${alpha_part}, SZ = ${dssize} ------------"
+        python eval_calib.py \
+            --corruption $CORRUPTION \
+            --models $OUTDIR/BinaryMNISTC-${dssize}-53-identity/ConvNet/$METHOD-alpha$alpha_part-sz$dssize-*
 
-#     done
-# done
+    done
+done
 
 # ------------------------------ SL (uniform prior) ----------------------------
 OUTDIR="zoo/sl/uniform-prior-alphavar"
@@ -98,67 +98,105 @@ do
             --corruption $CORRUPTION \
             --models $OUTDIR/BinaryMNISTC-${dssize}-53-identity/LeNet/$METHOD-alpha$alpha_part-sz$dssize-*
         
-        # ConvNet
-        echo "--------------- SL: ConvNet, alpha = ${alpha_part}, SZ = ${dssize} ------------"
-        python eval_calib.py \
-            --corruption $CORRUPTION \
-            --models $OUTDIR/BinaryMNISTC-${dssize}-53-identity/ConvNet/$METHOD-alpha$alpha_part-sz$dssize-*
+        # # ConvNet
+        # echo "--------------- SL: ConvNet, alpha = ${alpha_part}, SZ = ${dssize} ------------"
+        # python eval_calib.py \
+        #     --corruption $CORRUPTION \
+        #     --models $OUTDIR/BinaryMNISTC-${dssize}-53-identity/ConvNet/$METHOD-alpha$alpha_part-sz$dssize-*
     done
 done
 
 
-# # ---------------------------------- MFVI --------------------------------------
-# OUTDIR="zoo/mfvi"
-# METHOD="mfvi"
-# for dssize in 8000 1000
-# do
-#     # LeNet
-#     echo "--------------- MFVI: LeNet, SZ = ${dssize} ------------"
-#     python eval_calib.py \
-#         --corruption $CORRUPTION \
-#         --models $OUTDIR/BinaryMNISTC-${dssize}-53-identity/LeNet/$METHOD-sz$dssize-*
+# ---------------------------------- MFVI --------------------------------------
+OUTDIR="zoo/mfvi"
+METHOD="mfvi"
+for dssize in 8000 1000
+do
+    # LeNet
+    echo "--------------- MFVI: LeNet, SZ = ${dssize} ------------"
+    python eval_calib.py \
+        --corruption $CORRUPTION \
+        --models $OUTDIR/BinaryMNISTC-${dssize}-53-identity/LeNet/$METHOD-sz$dssize-*
     
-#     # ConvNet
-#     echo "--------------- MFVI: ConvNet, SZ = ${dssize} ------------"
-#     python eval_calib.py \
-#         --corruption $CORRUPTION \
-#         --models $OUTDIR/BinaryMNISTC-${dssize}-53-identity/ConvNet/$METHOD-sz$dssize-*
-# done
+    # ConvNet
+    echo "--------------- MFVI: ConvNet, SZ = ${dssize} ------------"
+    python eval_calib.py \
+        --corruption $CORRUPTION \
+        --models $OUTDIR/BinaryMNISTC-${dssize}-53-identity/ConvNet/$METHOD-sz$dssize-*
+done
 
 
-# # ----------------------------------- LS ---------------------------------------
-# OUTDIR="zoo/ls"
-# METHOD="ls"
-# for dssize in 8000 1000
-# do
-#     # LeNet
-#     echo "--------------- LS: LeNet, SZ = ${dssize} ------------"
-#     python eval_calib.py \
-#         --corruption $CORRUPTION \
-#         --models $OUTDIR/BinaryMNISTC-${dssize}-53-identity/LeNet/$METHOD-sz$dssize-*
+# ----------------------------------- LS ---------------------------------------
+OUTDIR="zoo/ls"
+METHOD="ls"
+for dssize in 8000 1000
+do
+    # LeNet
+    echo "--------------- LS: LeNet, SZ = ${dssize} ------------"
+    python eval_calib.py \
+        --corruption $CORRUPTION \
+        --models $OUTDIR/BinaryMNISTC-${dssize}-53-identity/LeNet/$METHOD-sz$dssize-*
     
-#     # ConvNet
-#     echo "--------------- LS: ConvNet, SZ = ${dssize} ------------"
-#     python eval_calib.py \
-#         --corruption $CORRUPTION \
-#         --models $OUTDIR/BinaryMNISTC-${dssize}-53-identity/ConvNet/$METHOD-sz$dssize-*
-# done
+    # ConvNet
+    echo "--------------- LS: ConvNet, SZ = ${dssize} ------------"
+    python eval_calib.py \
+        --corruption $CORRUPTION \
+        --models $OUTDIR/BinaryMNISTC-${dssize}-53-identity/ConvNet/$METHOD-sz$dssize-*
+done
 
 
-# # ----------------------------------- EDL --------------------------------------
-# OUTDIR="zoo/edl"
-# METHOD="edl"
-# for dssize in 8000 1000
-# do
-#     # LeNet
-#     echo "--------------- EDL: LeNet, SZ = ${dssize} ------------"
-#     python eval_calib.py \
-#         --corruption $CORRUPTION \
-#         --models $OUTDIR/BinaryMNISTC-${dssize}-53-identity/LeNetEDL/$METHOD-sz$dssize-*
+# ----------------------------------- EDL --------------------------------------
+OUTDIR="zoo/edl"
+METHOD="edl"
+for dssize in 8000 1000
+do
+    # LeNet
+    echo "--------------- EDL: LeNet, SZ = ${dssize} ------------"
+    python eval_calib.py \
+        --corruption $CORRUPTION \
+        --models $OUTDIR/BinaryMNISTC-${dssize}-53-identity/LeNetEDL/$METHOD-sz$dssize-*
     
-#     # ConvNet
-#     echo "--------------- EDL: ConvNet, SZ = ${dssize} ------------"
-#     python eval_calib.py \
-#         --corruption $CORRUPTION \
-#         --models $OUTDIR/BinaryMNISTC-${dssize}-53-identity/ConvNetEDL/$METHOD-sz$dssize-*
-# done
+    # ConvNet
+    echo "--------------- EDL: ConvNet, SZ = ${dssize} ------------"
+    python eval_calib.py \
+        --corruption $CORRUPTION \
+        --models $OUTDIR/BinaryMNISTC-${dssize}-53-identity/ConvNetEDL/$METHOD-sz$dssize-*
+done
+
+
+# ---------------------------- EDL + computed ----------------------------------
+OUTDIR="zoo/edl/computed-prior"
+METHOD="edl"
+for dssize in 8000 1000
+do
+    # LeNet
+    echo "--------------- EDL: LeNet, SZ = ${dssize} ------------"
+    python eval_calib.py \
+        --corruption $CORRUPTION \
+        --models $OUTDIR/BinaryMNISTC-${dssize}-53-identity/LeNetEDL/$METHOD-sz$dssize-*
+    
+    # ConvNet
+    echo "--------------- EDL: ConvNet, SZ = ${dssize} ------------"
+    python eval_calib.py \
+        --corruption $CORRUPTION \
+        --models $OUTDIR/BinaryMNISTC-${dssize}-53-identity/ConvNetEDL/$METHOD-sz$dssize-*
+done
+
+
+# ---------------------------- EDL + 0.1 ---------------------------------------
+OUTDIR="zoo/edl/skewed-prior"
+METHOD="edl"
+for dssize in 8000 1000
+do
+    # LeNet
+    echo "--------------- EDL: LeNet, SZ = ${dssize} ------------"
+    python eval_calib.py \
+        --corruption $CORRUPTION \
+        --models $OUTDIR/BinaryMNISTC-${dssize}-53-identity/LeNetEDL/$METHOD-sz$dssize-*
+    
+    # ConvNet
+    echo "--------------- EDL: ConvNet, SZ = ${dssize} ------------"
+    python eval_calib.py \
+        --corruption $CORRUPTION \
+        --models $OUTDIR/BinaryMNISTC-${dssize}-53-identity/ConvNetEDL/$METHOD-sz$dssize-*
+done
--- slurm-scripts/submit_mnistc_models.sh
+++ slurm-scripts/submit_mnistc_models.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --time=15:00:00
+#SBATCH --time=24:00:00
 #SBATCH --mem-per-cpu=4G
 #SBATCH --gres=gpu:1
 #SBATCH --exclude=dgx[1-7]
@@ -14,216 +14,215 @@ source activate bayes
 export DISABLE_PBAR=1
 
 
-# # -------------------------------- SL (auto prior) -----------------------------
-# OUTDIR="zoo/sl/auto-prior-alphavar"
-# MAX_STEPS=3000
-# METHOD="sl"
+# -------------------------------- SL (auto prior) -----------------------------
+OUTDIR="zoo/sl/auto-prior-alphavar"
+MAX_STEPS=3000
+METHOD="sl"
 
-# for alpha in 0.01 0.1 1.0 10.0 100.0 0.05 0.5 5.0 25.0 50.0 75.0
-# do
-#     for dssize in 8000 1000
-#     do
-#         alpha_part=`printf '%1.0e' $alpha`
+for alpha in 0.01 0.1 1.0 10.0 100.0 0.05 0.5 5.0 25.0 50.0 75.0
+do
+    for dssize in 8000 1000
+    do
+        alpha_part=`printf '%1.0e' $alpha`
 
-#         # LeNet
-#         python train.py \
-#             --method $METHOD --params auto,ea=0.98,alpha=$alpha \
-#             --dataset BinaryMNISTC \
-#             --ds-params size=$dssize,labels=53,corruption=identity \
-#             --model LeNet \
-#             --max-steps $MAX_STEPS \
-#             --batch-size 256 \
-#             --mc-samples 32 \
-#             --outdir $OUTDIR \
-#             --prefix $METHOD-alpha$alpha_part-sz$dssize-$SLURM_ARRAY_TASK_ID
+        # LeNet
+        python train.py \
+            --method $METHOD --params auto,ea=0.98,alpha=$alpha \
+            --dataset BinaryMNISTC \
+            --ds-params size=$dssize,labels=53,corruption=identity \
+            --model LeNet \
+            --max-steps $MAX_STEPS \
+            --batch-size 256 \
+            --mc-samples 32 \
+            --outdir $OUTDIR \
+            --prefix $METHOD-alpha$alpha_part-sz$dssize-$SLURM_ARRAY_TASK_ID
 
-#         # ConvNet
-#         python train.py \
-#             --method $METHOD --params auto,ea=0.98,alpha=$alpha \
-#             --dataset BinaryMNISTC \
-#             --ds-params size=$dssize,labels=53,corruption=identity \
-#             --model ConvNet \
-#             --max-steps $MAX_STEPS \
-#             --batch-size 256 \
-#             --mc-samples 32 \
-#             --outdir $OUTDIR \
-#             --prefix $METHOD-alpha$alpha_part-sz$dssize-$SLURM_ARRAY_TASK_ID
-#     done
-# done
-
-
-# # ------------------------------ SL (uniform prior) ----------------------------
-# OUTDIR="zoo/sl/uniform-prior-alphavar"
-# MAX_STEPS=3000
-# METHOD="sl"
-
-# # for alpha in 0.01 0.1 1.0 10.0 100.0 0.05 0.5 5.0 25.0 50.0 75.0
-# for alpha in 0.05 0.5 5.0 25.0 50.0 75.0
-# do
-#     for dssize in 8000 1000
-#     do
-#         alpha_part=`printf '%1.0e' $alpha`
-
-#         # LeNet
-#         python train.py \
-#             --method $METHOD --params beta,a=1,b=1,alpha=$alpha \
-#             --dataset BinaryMNISTC \
-#             --ds-params size=$dssize,labels=53,corruption=identity \
-#             --model LeNet \
-#             --max-steps $MAX_STEPS \
-#             --batch-size 256 \
-#             --mc-samples 32 \
-#             --outdir $OUTDIR \
-#             --prefix $METHOD-alpha$alpha_part-sz$dssize-$SLURM_ARRAY_TASK_ID
-
-#         # ConvNet
-#         python train.py \
-#             --method $METHOD --params beta,a=1,b=1,alpha=$alpha \
-#             --dataset BinaryMNISTC \
-#             --ds-params size=$dssize,labels=53,corruption=identity \
-#             --model ConvNet \
-#             --max-steps $MAX_STEPS \
-#             --batch-size 256 \
-#             --mc-samples 32 \
-#             --outdir $OUTDIR \
-#             --prefix $METHOD-alpha$alpha_part-sz$dssize-$SLURM_ARRAY_TASK_ID
-#     done
-# done
+        # ConvNet
+        python train.py \
+            --method $METHOD --params auto,ea=0.98,alpha=$alpha \
+            --dataset BinaryMNISTC \
+            --ds-params size=$dssize,labels=53,corruption=identity \
+            --model ConvNet \
+            --max-steps $MAX_STEPS \
+            --batch-size 256 \
+            --mc-samples 32 \
+            --outdir $OUTDIR \
+            --prefix $METHOD-alpha$alpha_part-sz$dssize-$SLURM_ARRAY_TASK_ID
+    done
+done
 
 
-# # -------------------------------- SL (0.5 prior) ------------------------------
-# OUTDIR="zoo/sl/half-prior-alphavar"
-# MAX_STEPS=3000
-# METHOD="sl"
+# ------------------------------ SL (uniform prior) ----------------------------
+OUTDIR="zoo/sl/uniform-prior-alphavar"
+MAX_STEPS=3000
+METHOD="sl"
 
-# # for alpha in 0.01 0.1 1.0 10.0 100.0 0.05 0.5 
-# for alpha in 5.0 25.0 50.0 75.0
-# do
-#     for dssize in 8000 1000
-#     do
-#         alpha_part=`printf '%1.0e' $alpha`
+for alpha in 0.01 0.1 1.0 10.0 100.0 0.05 0.5 5.0 25.0 50.0 75.0
+do
+    for dssize in 8000 1000
+    do
+        alpha_part=`printf '%1.0e' $alpha`
 
-#         # LeNet
-#         python train.py \
-#             --method $METHOD --params beta,a=0.5,b=0.5,alpha=$alpha \
-#             --dataset BinaryMNISTC \
-#             --ds-params size=$dssize,labels=53,corruption=identity \
-#             --model LeNet \
-#             --max-steps $MAX_STEPS \
-#             --batch-size 256 \
-#             --mc-samples 32 \
-#             --outdir $OUTDIR \
-#             --prefix $METHOD-alpha$alpha_part-sz$dssize-$SLURM_ARRAY_TASK_ID
+        # LeNet
+        python train.py \
+            --method $METHOD --params beta,a=1,b=1,alpha=$alpha \
+            --dataset BinaryMNISTC \
+            --ds-params size=$dssize,labels=53,corruption=identity \
+            --model LeNet \
+            --max-steps $MAX_STEPS \
+            --batch-size 256 \
+            --mc-samples 32 \
+            --outdir $OUTDIR \
+            --prefix $METHOD-alpha$alpha_part-sz$dssize-$SLURM_ARRAY_TASK_ID
 
-#         # ConvNet
-#         python train.py \
-#             --method $METHOD --params beta,a=0.5,b=0.5,alpha=$alpha \
-#             --dataset BinaryMNISTC \
-#             --ds-params size=$dssize,labels=53,corruption=identity \
-#             --model ConvNet \
-#             --max-steps $MAX_STEPS \
-#             --batch-size 256 \
-#             --mc-samples 32 \
-#             --outdir $OUTDIR \
-#             --prefix $METHOD-alpha$alpha_part-sz$dssize-$SLURM_ARRAY_TASK_ID
-#     done
-# done
-
-
-# # ---------------------------------- MFVI --------------------------------------
-# OUTDIR="zoo/mfvi"
-# MAX_STEPS=3000
-# METHOD="mfvi"
-# for dssize in 8000 1000
-# do
-#     # LeNet
-#     python train.py \
-#         --method $METHOD \
-#         --dataset BinaryMNISTC \
-#         --ds-params size=$dssize,labels=53,corruption=identity \
-#         --model LeNet \
-#         --max-steps $MAX_STEPS \
-#         --batch-size 256 \
-#         --mc-samples 32 \
-#         --outdir $OUTDIR \
-#         --prefix $METHOD-sz$dssize-$SLURM_ARRAY_TASK_ID
-
-#     # ConvNet
-#     python train.py \
-#         --method $METHOD \
-#         --dataset BinaryMNISTC \
-#         --ds-params size=$dssize,labels=53,corruption=identity \
-#         --model ConvNet \
-#         --max-steps $MAX_STEPS \
-#         --batch-size 256 \
-#         --mc-samples 32 \
-#         --outdir $OUTDIR \
-#         --prefix $METHOD-sz$dssize-$SLURM_ARRAY_TASK_ID
-# done
+        # ConvNet
+        python train.py \
+            --method $METHOD --params beta,a=1,b=1,alpha=$alpha \
+            --dataset BinaryMNISTC \
+            --ds-params size=$dssize,labels=53,corruption=identity \
+            --model ConvNet \
+            --max-steps $MAX_STEPS \
+            --batch-size 256 \
+            --mc-samples 32 \
+            --outdir $OUTDIR \
+            --prefix $METHOD-alpha$alpha_part-sz$dssize-$SLURM_ARRAY_TASK_ID
+    done
+done
 
 
-# # ------------------------------------- LS -------------------------------------
-# OUTDIR="zoo/ls"
-# MAX_STEPS=3000
-# METHOD="ls"
-# for dssize in 8000 1000
-# do
-#     # LeNet
-#     python train.py \
-#         --method $METHOD \
-#         --dataset BinaryMNISTC \
-#         --ds-params size=$dssize,labels=53,corruption=identity \
-#         --model LeNet \
-#         --max-steps $MAX_STEPS \
-#         --batch-size 256 \
-#         --mc-samples 32 \
-#         --outdir $OUTDIR \
-#         --prefix $METHOD-sz$dssize-$SLURM_ARRAY_TASK_ID
+# -------------------------------- SL (0.5 prior) ------------------------------
+OUTDIR="zoo/sl/half-prior-alphavar"
+MAX_STEPS=3000
+METHOD="sl"
 
-#     # ConvNet
-#     python train.py \
-#         --method $METHOD \
-#         --dataset BinaryMNISTC \
-#         --ds-params size=$dssize,labels=53,corruption=identity \
-#         --model ConvNet \
-#         --max-steps $MAX_STEPS \
-#         --batch-size 256 \
-#         --mc-samples 32 \
-#         --outdir $OUTDIR \
-#         --prefix $METHOD-sz$dssize-$SLURM_ARRAY_TASK_ID
-# done
+for alpha in 0.01 0.1 1.0 10.0 100.0 0.05 0.5 5.0 25.0 50.0 75.0
+do
+    # for dssize in 8000 1000
+    for dssize in 1000
+    do
+        alpha_part=`printf '%1.0e' $alpha`
+
+        # LeNet
+        python train.py \
+            --method $METHOD --params beta,a=0.5,b=0.5,alpha=$alpha \
+            --dataset BinaryMNISTC \
+            --ds-params size=$dssize,labels=53,corruption=identity \
+            --model LeNet \
+            --max-steps $MAX_STEPS \
+            --batch-size 256 \
+            --mc-samples 32 \
+            --outdir $OUTDIR \
+            --prefix $METHOD-alpha$alpha_part-sz$dssize-$SLURM_ARRAY_TASK_ID
+
+        # ConvNet
+        python train.py \
+            --method $METHOD --params beta,a=0.5,b=0.5,alpha=$alpha \
+            --dataset BinaryMNISTC \
+            --ds-params size=$dssize,labels=53,corruption=identity \
+            --model ConvNet \
+            --max-steps $MAX_STEPS \
+            --batch-size 256 \
+            --mc-samples 32 \
+            --outdir $OUTDIR \
+            --prefix $METHOD-alpha$alpha_part-sz$dssize-$SLURM_ARRAY_TASK_ID
+    done
+done
 
 
-# # ------------------------------------- EDL -------------------------------------
-# OUTDIR="zoo/edl"
-# MAX_STEPS=3000
-# METHOD="edl"
-# for dssize in 8000 1000
-# do
-#     # LeNet
-#     python train.py \
-#         --method $METHOD \
-#         --dataset BinaryMNISTC \
-#         --ds-params size=$dssize,labels=53,corruption=identity \
-#         --model LeNetEDL \
-#         --max-steps $MAX_STEPS \
-#         --batch-size 256 \
-#         --mc-samples 32 \
-#         --outdir $OUTDIR \
-#         --prefix $METHOD-sz$dssize-$SLURM_ARRAY_TASK_ID
+# ---------------------------------- MFVI --------------------------------------
+OUTDIR="zoo/mfvi"
+MAX_STEPS=3000
+METHOD="mfvi"
+for dssize in 8000 1000
+do
+    # LeNet
+    python train.py \
+        --method $METHOD \
+        --dataset BinaryMNISTC \
+        --ds-params size=$dssize,labels=53,corruption=identity \
+        --model LeNet \
+        --max-steps $MAX_STEPS \
+        --batch-size 256 \
+        --mc-samples 32 \
+        --outdir $OUTDIR \
+        --prefix $METHOD-sz$dssize-$SLURM_ARRAY_TASK_ID
 
-#     # ConvNet
-#     python train.py \
-#         --method $METHOD \
-#         --dataset BinaryMNISTC \
-#         --ds-params size=$dssize,labels=53,corruption=identity \
-#         --model ConvNetEDL \
-#         --max-steps $MAX_STEPS \
-#         --batch-size 256 \
-#         --mc-samples 32 \
-#         --outdir $OUTDIR \
-#         --prefix $METHOD-sz$dssize-$SLURM_ARRAY_TASK_ID
-# done
+    # ConvNet
+    python train.py \
+        --method $METHOD \
+        --dataset BinaryMNISTC \
+        --ds-params size=$dssize,labels=53,corruption=identity \
+        --model ConvNet \
+        --max-steps $MAX_STEPS \
+        --batch-size 256 \
+        --mc-samples 32 \
+        --outdir $OUTDIR \
+        --prefix $METHOD-sz$dssize-$SLURM_ARRAY_TASK_ID
+done
+
+
+# ------------------------------------- LS -------------------------------------
+OUTDIR="zoo/ls"
+MAX_STEPS=3000
+METHOD="ls"
+for dssize in 8000 1000
+do
+    # LeNet
+    python train.py \
+        --method $METHOD \
+        --dataset BinaryMNISTC \
+        --ds-params size=$dssize,labels=53,corruption=identity \
+        --model LeNet \
+        --max-steps $MAX_STEPS \
+        --batch-size 256 \
+        --mc-samples 32 \
+        --outdir $OUTDIR \
+        --prefix $METHOD-sz$dssize-$SLURM_ARRAY_TASK_ID
+
+    # ConvNet
+    python train.py \
+        --method $METHOD \
+        --dataset BinaryMNISTC \
+        --ds-params size=$dssize,labels=53,corruption=identity \
+        --model ConvNet \
+        --max-steps $MAX_STEPS \
+        --batch-size 256 \
+        --mc-samples 32 \
+        --outdir $OUTDIR \
+        --prefix $METHOD-sz$dssize-$SLURM_ARRAY_TASK_ID
+done
+
+
+# ------------------------------------- EDL -------------------------------------
+OUTDIR="zoo/edl"
+MAX_STEPS=3000
+METHOD="edl"
+for dssize in 8000 1000
+do
+    # LeNet
+    python train.py \
+        --method $METHOD \
+        --dataset BinaryMNISTC \
+        --ds-params size=$dssize,labels=53,corruption=identity \
+        --model LeNetEDL \
+        --max-steps $MAX_STEPS \
+        --batch-size 256 \
+        --mc-samples 32 \
+        --outdir $OUTDIR \
+        --prefix $METHOD-sz$dssize-$SLURM_ARRAY_TASK_ID
+
+    # ConvNet
+    python train.py \
+        --method $METHOD \
+        --dataset BinaryMNISTC \
+        --ds-params size=$dssize,labels=53,corruption=identity \
+        --model ConvNetEDL \
+        --max-steps $MAX_STEPS \
+        --batch-size 256 \
+        --mc-samples 32 \
+        --outdir $OUTDIR \
+        --prefix $METHOD-sz$dssize-$SLURM_ARRAY_TASK_ID
+done
 
 # --------------------------------- EDL + computed -----------------------------
 OUTDIR="zoo/edl/computed-prior"
