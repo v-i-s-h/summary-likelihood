b06d9d95a2cdcd0da7cb17c9d591fc7392820170
v-i-s-h <get_vichu@yahoo.com>
2022-09-15 15:47:44 +0300

    OOD eval - initial

---
+++ eval_ood.py
@@ -0,0 +1,216 @@
+# Evaluate model(s) on given OOD dataset
+
+import os
+import pickle
+import json
+import glob
+import argparse
+import copy
+from datetime import datetime
+
+from tqdm import tqdm
+
+import numpy as np
+import torch
+import torch.optim as optim
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+from torch.utils.data.sampler import SubsetRandomSampler
+
+import pandas as pd
+
+import models
+import datasets
+import transforms
+
+
+def get_predictions(model, dataloader, msg="Getting predictions"):
+    device = next(model.parameters()).device
+    tqdm_params = {
+        'leave': False,
+        'desc': msg,
+        'disable': True if os.environ.get('SLURM_JOB_ID', False) else False
+    }
+
+    y_true = None
+    scores = None
+    
+    model.eval()
+    with torch.no_grad():
+        for x, y in tqdm(dataloader, **tqdm_params):
+            x = x.to(device)
+            y = y.to(device)
+
+            ypred = []
+            for mc_run in range(128): # TODO: Adjustable MC sampling
+                _ypred = model.get_softmax(x)
+                ypred.append(_ypred)
+
+            score = torch.mean(torch.stack(ypred), dim=0)
+            
+            if y_true is None:
+                y_true = y.detach()
+            else:
+                y_true = torch.concat([y_true, y.detach()])
+            if scores is None:
+                scores = score.detach()
+            else:
+                scores = torch.concat([scores, score.detach()])
+    model.train()
+
+    return y_true, scores
+
+
+def get_model_predictions(model, valloader, testloader, n_bins=10):
+    # Get predictions from validation set and calibrate models
+    y_true_val, scores_val = get_predictions(model, valloader)
+    
+    ### Evaluate calibration of testloader
+    y_true_test, scores_test = get_predictions(model, testloader)
+
+    return {
+        'val': [ y_true_val, scores_val],
+        'test': [ y_true_test, scores_test]
+    }
+
+
+def compute_mean_entropy(p):
+    return torch.sum(-torch.log(p) * p, dim=1).mean()
+
+
+def evaluate_model_performance(preds, n_bins=10):
+    # To store results
+    results = {}
+    
+    # Get predictions from validation set and calibrate models
+    y_true_val, scores_val = preds['val']
+    
+    ### Evaluate calibration of testloader
+    y_true_test, scores_test = preds['test']
+    
+
+    results['ent_val'] = compute_mean_entropy(scores_val).detach().item()
+    results['ent_test'] = compute_mean_entropy(scores_test).detach().item()
+
+    return results
+
+
+def run_evaluation(model_str, ckpt_file, transform, dataset):
+    # Setup input transform
+    x_transform = getattr(transforms, transform)() # returns a transformation object
+
+    # Load dataset
+    DatasetClass = getattr(datasets, dataset)
+
+    valset = DatasetClass(split='val', transform=x_transform)
+    valloader = DataLoader(dataset=valset, batch_size=256, shuffle=True)
+
+    testset = DatasetClass(split='test', transform=x_transform)
+    testloader = DataLoader(dataset=testset, batch_size=256, shuffle=False)
+
+    n = len(testset)
+    indices = torch.randperm(n)
+    val_size = int(0.25 * n)
+    val_indices = indices[:val_size]
+    test_indices = indices[val_size:]
+
+    valloader = DataLoader(dataset=testset, batch_size=256, 
+                            sampler=SubsetRandomSampler(val_indices))
+    testloader = DataLoader(dataset=testset, batch_size=256, 
+                            sampler=SubsetRandomSampler(test_indices))
+    
+
+    # CUDA config
+    device = 'cpu'
+    if torch.cuda.is_available():
+        device = 'cuda'
+    
+    # Load saved weights
+    checkpoint = torch.load(ckpt_file, map_location=torch.device(device))
+    # Clean up state dict to have only model params
+    state_dict = checkpoint['state_dict']
+    model_weights = {}
+    for k, v in state_dict.items():
+        if k.startswith('model.'):
+            # remove `model.` from start and add to state dict
+            model_weights[k[6:]] = v
+
+    # Get last layer's bias parameter to identify the number of labels
+    # the model is trained for
+    n_labels = model_weights[list(model_weights.keys())[-1]].shape[0]
+
+    # Build model
+    ModelClass = getattr(models, model_str)
+    model = ModelClass(n_labels).to(device)
+    model.load_state_dict(model_weights)
+    # model.load_state_dict(torch.load(weights_file, map_location=torch.device(device)))
+
+    # Get predictions from model
+    preds = get_model_predictions(model, valloader, testloader)
+    del model # Free model
+    preds['n_classes'] = n_labels
+
+    r = evaluate_model_performance(preds)
+
+    return r
+
+
+def evaluate_model_in_dir(model_dir, dataset):
+    config_path = os.path.join(model_dir, "config.json")
+    weights_path = os.path.join(model_dir, "model.pth")
+
+    # Load experiment configuration
+    with open(config_path) as fp:
+        config = json.load(fp)
+
+    # Get the best weights file
+    ckpt_path = glob.glob(model_dir + '/step=*.ckpt')[-1]
+
+    
+    r = run_evaluation(
+            config['model'], 
+            ckpt_path, 
+            config['transform'],
+            dataset)
+
+    return r  
+
+
+def main():
+    # Timestamp for experiment
+    timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
+
+    # Set up command line args parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset', type=str, required=True, default=None)
+    parser.add_argument('--models', nargs='+', required=True)
+    args = parser.parse_args()
+
+    model_dirs = args.models
+    if not isinstance(model_dirs, list):
+        model_dirs = list(model_dirs)
+
+    results = []
+
+    for i, model_dir in tqdm(enumerate(model_dirs), desc="Models", total=len(model_dirs)):
+        print("INFO: Model -", model_dir)
+        model_results = []
+        for j in range(1):
+            r = {'model_id': model_dir, 'round': j}
+            _r = evaluate_model_in_dir(model_dir, args.dataset)
+            r.update(_r)
+            model_results.append(r)
+        
+        results_file = os.path.join(model_dir, 
+                            "ood_results_{}.pkl".format(args.dataset))
+        with open(results_file, 'wb') as f:
+            pickle.dump(model_results, f)
+        results.extend(model_results)
+
+    df_results = pd.DataFrame(results)
+    print(df_results)
+    
+
+if __name__ == "__main__":
+    main()
