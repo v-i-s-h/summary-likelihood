63a9f8c7e5bf331b1dd9e6f888bf71588306c304
v-i-s-h <get_vichu@yahoo.com>
2022-09-27 18:06:13 +0300

    CIFAR-10-C downlad script

--- data/download.sh
+++ data/download.sh
@@ -8,21 +8,27 @@ wget -O mnist_c.zip https://zenodo.org/record/3239543/files/mnist_c.zip?download
 unzip mnist_c.zip
 rm mnist_c.zip
 
-# ChestMNIST
-### ChestMNIST
-echo "Downloading ChestMNIST"
-curl -o chestmnist.npz https://zenodo.org/record/5208230/files/chestmnist.npz
+# CIFAR10-C
+echo "Downloading CIFAR10-C dataset"
+wget -O CIFAR-10-C.tar https://zenodo.org/record/2535967/files/CIFAR-10-C.tar
+tar -xf CIFAR-10-C.tar
+rm CIFAR-10-C.tar
 
-# BloodMNIST
-echo "Downloading BloodMNIST"
-curl -o bloodmnist.npz https://zenodo.org/record/5208230/files/bloodmnist.npz
+# # ChestMNIST
+# ### ChestMNIST
+# echo "Downloading ChestMNIST"
+# curl -o chestmnist.npz https://zenodo.org/record/5208230/files/chestmnist.npz
 
-# PathMNIST
-echo "Downloading PathMNIST"
-curl -o pathmnist.npz https://zenodo.org/record/5208230/files/pathmnist.npz
+# # BloodMNIST
+# echo "Downloading BloodMNIST"
+# curl -o bloodmnist.npz https://zenodo.org/record/5208230/files/bloodmnist.npz
 
-# TissueMNIST
-echo "Downloading TissueMNIST"
-curl -o tissuemnist.npz https://zenodo.org/record/5208230/files/tissuemnist.npz
+# # PathMNIST
+# echo "Downloading PathMNIST"
+# curl -o pathmnist.npz https://zenodo.org/record/5208230/files/pathmnist.npz
+
+# # TissueMNIST
+# echo "Downloading TissueMNIST"
+# curl -o tissuemnist.npz https://zenodo.org/record/5208230/files/tissuemnist.npz
 
 cd $PREV_DIR
