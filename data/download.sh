7cff81713e414e6f769e41738703a3550b655f85
v-i-s-h <get_vichu@yahoo.com>
2023-01-27 18:45:14 +0200

    Cleaned download script

--- data/download.sh
+++ data/download.sh
@@ -2,17 +2,17 @@ PREV_DIR=`pwd`
 cd "${0%/*}"
 
 
-# # MNIST-C
-# echo "Downloading MNIST-C dataset"
-# wget -O mnist_c.zip https://zenodo.org/record/3239543/files/mnist_c.zip?download=1
-# unzip mnist_c.zip
-# rm mnist_c.zip
+# MNIST-C
+echo "Downloading MNIST-C dataset"
+wget -O mnist_c.zip https://zenodo.org/record/3239543/files/mnist_c.zip?download=1
+unzip mnist_c.zip
+rm mnist_c.zip
 
-# # CIFAR10-C
-# echo "Downloading CIFAR10-C dataset"
-# wget -O CIFAR-10-C.tar https://zenodo.org/record/2535967/files/CIFAR-10-C.tar
-# tar -xf CIFAR-10-C.tar
-# rm CIFAR-10-C.tar
+# CIFAR10-C
+echo "Downloading CIFAR10-C dataset"
+wget -O CIFAR-10-C.tar https://zenodo.org/record/2535967/files/CIFAR-10-C.tar
+tar -xf CIFAR-10-C.tar
+rm CIFAR-10-C.tar
 
 # SST
 echo "Downloading SST"
