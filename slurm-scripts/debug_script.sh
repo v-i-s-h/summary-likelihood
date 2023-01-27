651954630991cebe26204395be609ab8a2251591
v-i-s-h <get_vichu@yahoo.com>
2023-01-27 17:48:07 +0200

    Updates

--- slurm-scripts/debug_script.sh
+++ slurm-scripts/debug_script.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-#SBATCH --time=0:30:00
-#SBATCH --mem=1G
-#SBATCH --cpus-per-task=4
-#SBATCH --output=logs/debug-job-%A.out
-#SBATCH --error=logs/debug-job-%A.err
+#SBATCH --time=2:00:00
+#SBATCH --mem-per-cpu=4G
+#SBATCH --gres=gpu:1
+#SBATCH --exclude=dgx[1-7]
+#SBATCH --output=logs/job-test-%A.out
+#SBATCH --error=logs/job-test-%A.err
+
 
 module purge
 module load miniconda
@@ -59,14 +61,14 @@ export DISABLE_PBAR=1
 #     --prefix sst-test
 
 
-python train.py \
-    --method sl --params auto,ea=0.98,alpha=2000,adahist \
-    --dataset SSTBERT --transform normalize_x_sst \
-    --model SSTNet \
-    --max-steps 2000 \
-    --batch-size 256 \
-    --outdir zoo/test/ \
-    --prefix sst-test
+# python train.py \
+#     --method sl --params auto,ea=0.98,alpha=2000,adahist \
+#     --dataset SSTBERT --transform normalize_x_sst \
+#     --model SSTNet \
+#     --max-steps 2000 \
+#     --batch-size 256 \
+#     --outdir zoo/test/ \
+#     --prefix sst-test
 
 
 # python train.py \
@@ -77,3 +79,95 @@ python train.py \
 #     --batch-size 256 \
 #     --outdir zoo/test/ \
 #     --prefix sst-test
+
+
+# python train.py \
+#     --method sgd \
+#     --dataset CIFAR10 --transform normalize_x_cifar_v2 \
+#     --model VGG11Deterministic \
+#     --max-steps 100 \
+#     --batch-size 256 \
+#     --outdir zoo/test/ \
+#     --prefix sgd-test
+
+# general config
+# MAX_STEPS=5000
+
+# python train.py \
+#     --method sgd \
+#     --dataset CIFAR10 --transform normalize_x_cifar_v2 \
+#     --model VGG11Deterministic --model-params pretrained,head-only \
+#     --max-steps $MAX_STEPS \
+#     --batch-size 256 \
+#     --outdir zoo/test/ \
+#     --prefix sgd-prehead
+
+
+# python train.py \
+#     --method sgd \
+#     --dataset CIFAR10 --transform cifar_da_x \
+#     --model VGG11Deterministic \
+#     --max-steps $MAX_STEPS \
+#     --batch-size 256 \
+#     --outdir zoo/test/ \
+#     --prefix sgd-w-da
+
+# python train.py \
+#     --method sgdsl --params alpha=1000 \
+#     --dataset CIFAR10 --transform cifar_da_x \
+#     --model VGG11Deterministic \
+#     --max-steps $MAX_STEPS \
+#     --batch-size 256 \
+#     --outdir zoo/test/ \
+#     --prefix sgd-sl
+
+# python train.py \
+#     --method sgdsl --params alpha=1000 \
+#     --dataset CIFAR10 --transform normalize_x_cifar_v2 \
+#     --model VGG11Deterministic \
+#     --max-steps $MAX_STEPS \
+#     --batch-size 256 \
+#     --outdir zoo/test/ \
+#     --prefix sgd-sl-noaug
+
+
+# OUTDIR="zoo/test/CIFAR10/VGG11Deterministic"
+# CORRUPTION="identity"
+# for level in {1..5}
+# do
+#     python eval_calib.py \
+#     --corruption $CORRUPTION-$level \
+#     --models ${OUTDIR}/sgd-*
+# done
+# python eval_calib.py \
+#     --corruption identity-1 \
+#     --models ${OUTDIR}/sgd-sl-noaug-*
+
+# echo "-------------------------------------------------------------------------"
+
+# python eval_ood.py \
+#     --ood SVHN \
+#     --models $OUTDIR/sgd-sl-noaug-*
+
+# python eval_ood_save_preds.py \
+#     --ood SVHN \
+#     --models zoo/multiclass/mfvi/CIFAR10Im/VGG11/mfvi-*
+
+# python eval_ood_save_preds.py \
+#     --ood SVHN \
+#     --models zoo/multiclass/slim/CIFAR10Im/VGG11/slim-alpha5e+02-*
+
+
+# python eval_ood_save_preds.py \
+#     --ood SVHN \
+#     --models zoo/multiclass/edl/uniform-prior/CIFAR10Im/VGG11EDL/edl-*
+
+
+python eval_ood_save_preds.py \
+    --ood SVHN \
+    --models zoo/multiclass/mfvi/CIFAR10/VGG11/mfvi-*
+
+python eval_ood_save_preds.py \
+    --ood SVHN \
+    --models zoo/multiclass/sl/CIFAR10/VGG11/sl-alpha5e+02-*
+
